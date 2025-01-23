909a84da40427f298a1f40c510922be8f001df4b
eserdeiro <eserdeiro@gmail.com>
2025-01-23 13:20:35 -0300

    commit to save changes

--- app/tabs/_layout.tsx
+++ app/tabs/_layout.tsx
@@ -1,11 +1,30 @@
 import { View, Text } from "react-native";
+import { FontAwesome } from "@expo/vector-icons";
 import React from "react";
+import { Tabs } from "expo-router";
 
 const TabsLayout = () => {
   return (
-    <View>
-      <Text>TabsLayout</Text>
-    </View>
+    <Tabs screenOptions={{ tabBarActiveTintColor: "purple" }}>
+      <Tabs.Screen
+        name="home/index"
+        options={{
+          title: "Home",
+          tabBarIcon: ({ color }) => (
+            <FontAwesome size={28} name="home" color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="favorites/index"
+        options={{
+          title: "Settings",
+          tabBarIcon: ({ color }) => (
+            <FontAwesome size={28} name="cog" color={color} />
+          ),
+        }}
+      />
+    </Tabs>
   );
 };
 
